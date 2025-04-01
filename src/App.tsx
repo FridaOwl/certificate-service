8ab120455faff76a0bac7c46d219121dd4241c29
FridaOwl <farvin.lori@gmail.com>
2025-04-01 22:08:09 +0300

    upd:app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 import BookingForm from "./components/BookingForm/BookingFormFields";
+import CancellationPage from "./components/CancellationPage/CancellationPage";
 import "./assets/styles/App.sass";
 
 const App: React.FC = () => {
@@ -10,7 +12,10 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<BookingForm onSubmit={handleSubmit} />} />
+          <Route path="cancellation" element={<CancellationPage />} />
+        </Route>
       </Routes>
     </Router>
   );
